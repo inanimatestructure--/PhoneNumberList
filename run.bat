2ba6f6a8b8513babdc337779d890f3f5d8083038
inanimatestructure-- <jakehollis425@gmail.com>
2021-07-21 16:36:59 -0300

    add

--- run.bat
+++ run.bat
@@ -4,6 +4,7 @@ ECHO RUN EXTRA DIRECTORY SEARCH ON POSTAL CODES
 ECHO ============================
 ECHO This may take a while .... 
 mkdir C:\phone_temp
+mkdir C:\phone_temp\real
 node --max-old-space-size=7168 postal_code_info.js C:\phone_temp\gp_phone.csv
 ECHO Search all directories. This may take A LOT longer...
 set num=2
@@ -14,8 +15,8 @@ if %num% == 3 goto close
 goto loop
 :close
 powershell -Command "Get-Content C:\phone_temp\*.csv | Add-Content C:\phone_temp\gp_final.csv"
-powershell -Command "Import-Csv C:\phone_temp\gp_final.csv | sort Name -Unique | Export-CSV -Path C:\phone_temp\gp_phone_list.csv -NoTypeInformation"
-powershell -Command "Dir C:\phone_temp\*.csv -Exclude C:\phone_temp\gp_phone_list.csv -R | del"
+powershell -Command "Import-Csv C:\phone_temp\gp_final.csv | sort Name -Unique | Export-CSV -Path C:\phone_temp\real\gp_phone_list.csv -NoTypeInformation"
+powershell -Command "Dir C:\phone_temp\*.csv -R | del"
 :: Example if you wanted to get rid of postal codes that were not part of your search
 :: Get-Content gp_phone_list.csv | Where{$_ -notmatch "B3K"} | Out-File gp2.csv
 :: Get-Content gp2.csv | Where{$_ -notmatch "B3L"} | Out-File gp3.csv
